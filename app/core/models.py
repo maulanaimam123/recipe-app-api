ad20e03863b21c27430a30adcae45e0fec07e846
maulanaimam123 <maulana.putro@gmail.com>
2022-12-06 00:04:39 +0800

    fixing linting

--- app/core/models.py
+++ app/core/models.py
@@ -11,10 +11,10 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager for users"""
-    def create_user(self, email, password = None, **extra_fields):
-        user = self.model(email = email, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
