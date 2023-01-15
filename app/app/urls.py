3b30af5a53864a3b2bdafba698001eb29db94ffe
maulanaimam123 <maulana.putro@gmail.com>
2023-01-16 03:09:47 +0800

    adding endpoint for recipe upload image

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -37,5 +37,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT,
+        document_root=settings.MEDIA_ROOT,
     )
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,6 +12,7 @@ from recipe.serializers import (
     RecipeDetailSerializer,
     TagSerializer,
     IngredientSerializer,
+    RecipeImageSerializer,
 )
 
 from rest_framework import (
@@ -19,7 +20,11 @@ from rest_framework import (
     authentication,
     permissions,
     mixins,
+    status,
 )
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class RecipeViewset(viewsets.ModelViewSet):
@@ -37,11 +42,25 @@ class RecipeViewset(viewsets.ModelViewSet):
         """Method to define serializer for endpoint action"""
         if (self.action == 'list'):
             return RecipeSerializer
+        elif (self.action == 'upload_image'):
+            return RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create new recipe, link with current user"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload_image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewset(
